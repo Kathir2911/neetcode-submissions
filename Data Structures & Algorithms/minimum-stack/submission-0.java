123814b7b0e4f933e5540abe29985b4a2cf3aa3f
Kathir G <146568182+Kathir2911@users.noreply.github.com>
2026-08-27 11:05:00 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,25 @@
+class MinStack {
+    
+    Deque<Integer> stack;
+
+    public MinStack() {
+        stack=new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+    }
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        PriorityQueue<Integer> minheap=new PriorityQueue<>(stack);
+        return minheap.poll();
+    }
+}
